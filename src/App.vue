71ed30598ff4574851734c9360f95981e603522c
lidoudoou <lidoudoou@gmail.com>
2022-05-20 21:36:38 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <my-header></my-header>
+  <router-view />
+</template>
+<script>
+import header from '@/components/header/header'
+export default {
+  components: {
+    MyHeader: header
+  }
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
