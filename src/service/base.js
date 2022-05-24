9810f5ccd653f7c2f57ed5d665f46b7ff40bc169
lidoudoou <lidoudoou@gmail.com>
2022-05-24 21:39:42 +0800

    backend service

---
+++ src/service/base.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+const ERR_OK = 0
+const baseURL = '/'
+
+axios.defaults.baseURL = baseURL
+
+export function get(url, params) {
+  return axios.get(url, { params }).then(res => {
+    const resData = res.data
+    if (resData.code === ERR_OK) {
+      return resData.results
+    }
+  })
+}
