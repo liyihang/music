49ea0fdd6861d898b79d17702d0e7f8240eb62bb
lidoudoou <lidoudoou@gmail.com>
2022-05-24 14:31:20 +0800

    fix sass loader version error

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+        @import "@/assets/scss/variable.scss";
+        @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  }
+}
