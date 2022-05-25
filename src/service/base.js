de325a86756341381bf34cd924ec23dbcfc10177
lidoudoou <lidoudoou@gmail.com>
2022-05-25 19:40:40 +0800

    recomend sliders

--- src/service/base.js
+++ src/service/base.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const ERR_OK = 0
-const baseURL = '/'
+const baseURL = process.env.NODE_ENV === 'production' ? 'http://ustbhuangyi.com/music-next/' : '/'
 
 axios.defaults.baseURL = baseURL
 
@@ -8,7 +8,9 @@ export function get(url, params) {
   return axios.get(url, { params }).then(res => {
     const resData = res.data
     if (resData.code === ERR_OK) {
-      return resData.results
+      return resData.result
     }
+  }).catch(e => {
+    console.log(e)
   })
 }
