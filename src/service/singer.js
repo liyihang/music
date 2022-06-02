d55cf1d424fc9c88694aff45617222618055672b
lidoudoou <lidoudoou@gmail.com>
2022-06-02 18:31:23 +0800

    singer request function

--- src/service/singer.js
+++ src/service/singer.js
@@ -1,5 +1,10 @@
 import { get } from './base'
 
+// get singer list
 export function getSingerList() {
   return get('/api/getSingerList')
+}
+// get singer detail
+export function getSingerDetail(singer) {
+  return get('/api/getSingerDetail', { mid: singer.mid })
 }
