39a4e3aca28e5e0a41f0bb83a9fc7467ad628cf7
lidoudoou <lidoudoou@gmail.com>
2022-06-04 23:26:16 +0800

    music list

--- src/views/singer-detail.vue
+++ src/views/singer-detail.vue
@@ -1,23 +1,50 @@
 <template>
-  <div class="singer-detail"></div>
+  <div class="singer-detail">
+    <music-list :songs="songs" :title="title" :pic="pic"></music-list>
+  </div>
 </template>
 
 <script>
 import { getSingerDetail } from '../service/singer'
 import { processSongs } from '../service/songs'
+import musicList from '@/components/music-list/music-list.vue'
 export default {
   name: 'singer-detail',
+  components: {
+    musicList
+  },
+  data() {
+    return {
+      songs: []
+    }
+  },
+  computed: {
+    pic() {
+      return this.singer && this.singer.pic
+    },
+    title() {
+      return this.singer && this.singer.name
+    }
+  },
   props: {
     singer: Object
   },
   async created() {
     const result = await getSingerDetail(this.singer)
-    const songs = await processSongs(result.songs)
-    console.log(songs)
+    this.songs = await processSongs(result.songs)
   }
 
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.singer-detail {
+  position: fixed;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: $color-background;
+}
 </style>
