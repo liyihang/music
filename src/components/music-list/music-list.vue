aa1b717cd7a0a29a29ea9e165f0c01a401a1bf97
lidoudoou <lidoudoou@gmail.com>
2022-06-04 23:02:28 +0800

    song-list

---
+++ src/components/music-list/music-list.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="music-list">
+    <div class="back">
+      <i class="icon-back"></i>
+    </div>
+    <h1 class="title"> {{ title }}</h1>
+    <div class="bg-image" :style="bgImageStyle">
+      <div class="filter"></div>
+    </div>
+    <scroll class="list">
+      <div class="song-list-wrapper">
+
+      </div>
+    </scroll>
+  </div>
+</template>
+<script>
+import Scroll from '@/components/base/scroll/scroll'
+export default {
+  name: 'music-list',
+  components: {
+    Scroll
+  }
+}
+</script>
+
+<style>
+</style>
