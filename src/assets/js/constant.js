511bec6cd7c5ed2cafcd28592fe03a73fd8a2a0f
lidoudoou <lidoudoou@gmail.com>
2022-06-11 18:15:59 +0800

    vuex state

--- src/assets/js/constant.js
+++ src/assets/js/constant.js
@@ -1,1 +1,12 @@
 export const SINGER_KEY = '__singer__'
+/**
+ * 播放模式
+ * 随机
+ * 循环
+ * 顺序
+ *  */
+export const PLAYMODE = {
+  sequenceList: 0,
+  loop: 1,
+  ramdom: 2
+}
---
+++ src/store/state.js
@@ -0,0 +1,9 @@
+import { PLAYMODE } from '@/assets/js/constant'
+const state = {
+  sequenceList: [],
+  playList: [],
+  playing: false,
+  playMode: PLAYMODE.sequence,
+  currentIndex: 0,
+  fullScreen: false
+}
