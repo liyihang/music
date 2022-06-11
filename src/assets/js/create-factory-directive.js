8aa02ba2a449bdc47eaa529dc3116ab2f2487cc5
lidoudoou <lidoudoou@gmail.com>
2022-06-11 17:44:26 +0800

    domjs

--- src/assets/js/create-factory-directive.js
+++ src/assets/js/create-factory-directive.js
@@ -1,25 +1,29 @@
 import { createApp } from 'vue'
-import Loading from './loading.vue'
 
-const loadingDirective = {
-  mounted(el, binding) {
-    const app = createApp(Loading)
-    const instance = app.mount(document.createElement('div'))
-    el.instance = instance
-    if (binding.value) {
-      append(el)
-    }
-  },
-  updated(el, binding) {
-    if (binding.value !== binding.oldValue) {
-      binding.value ? append(el) : remove(el)
+export default function createFactoryDirective(Comp) {
+  return {
+    mounted(el, binding) {
+      const app = createApp(Comp)
+      const instance = app.mount(document.createElement('div'))
+      el.instance = instance
+      const title = binding.arg
+      if (typeof title !== 'undefined') {
+        instance.setTitle(title)
+      }
+      if (binding.value) {
+        append(el)
+      }
+    },
+    updated(el, binding) {
+      if (binding.value !== binding.oldValue) {
+        binding.value ? append(el) : remove(el)
+      }
     }
   }
+  function append(el) {
+    el.appendChild(el.instance.$el)
+  }
+  function remove(el) {
+    el.removeChild(el.instance.$el)
+  }
 }
-function append(el) {
-  el.appendChild(el.instance.$el)
-}
-function remove(el) {
-  el.removeChild(el.instance.$el)
-}
-export default loadingDirective
