184f7ad4668d2fea5f18c61fa25c27e0da1ba048
lidoudoou <lidoudoou@gmail.com>
2022-06-21 21:17:45 +0800

    player favorite

--- src/assets/js/constant.js
+++ src/assets/js/constant.js
@@ -1,4 +1,6 @@
 export const SINGER_KEY = '__singer__'
+export const FAVORITE_KEY = '__favorite__'
+
 /**
  * 播放模式
  * 随机
--- src/components/player/useFavorite.js
+++ src/components/player/useFavorite.js
@@ -1,6 +1,38 @@
 import { useStore } from 'vuex'
 import { computed } from 'vue'
+import { save } from '@/assets/js/array-store'
+import { FAVORITE_KEY } from '@ /assets/js/constant'
 export default function useFavorite() {
   const store = useStore()
   const favoriteList = computed(() => store.state.favoriteList)
+
+  // favorite iocn 图标
+  function getFavoriteIcon(song) {
+    return isFavorite(song) ? 'icon-favorite' : 'icon-not-favorite'
+  }
+  // 歌曲收藏
+  function toggleFavorite(song) {
+    let list
+    if (isFavorite(song)) {
+      // remove
+    } else {
+      // save
+      list = save(song, FAVORITE_KEY, compare)
+    }
+    song.commit('setFavoriteList', list)
+    function compare(item) {
+      return item.id === song.id
+    }
+  }
+
+  // 获取收藏歌曲
+  function isFavorite(song) {
+    return (
+      favoriteList.value.findIndex((item) => {
+        return item.id === song.id
+      }) > -1
+    )
+  }
+
+  return { getFavoriteIcon }
 }
