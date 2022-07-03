7e4c31465100a65745cd64ef0fed2ad2c108a36c
lidoudoou <lidoudoou@gmail.com>
2022-07-04 00:26:02 +0800

    cd lyric mode change

--- src/components/player/use-middle-interactive.js
+++ src/components/player/use-middle-interactive.js
@@ -13,14 +13,55 @@ export default function useMiddleInterActive() {
     const left = currentView === 'cd' ? 0 : -window.innerWidth
     const offsetWidth = Math.min(0, Math.max(-window.innerWidth, left + deltaX))
     touch.percent = Math.abs(offsetWidth / window.innerWidth)
-    if (currentView == 'cd') {
+    if (currentView === 'cd') {
       if (touch.percent > 0.2) {
-        touc
+        currentShow.value = 'lyric'
+      } else {
+        currentShow.value = 'cd'
+      }
+    } else {
+      if (touch.percent < 0.8) {
+        currentShow.value = 'cd'
+      } else {
+        currentShow.value = 'lyric'
       }
     }
+    // edit style
+    middleLStyle.value = {
+      opacity: 1 - touch.percent,
+      transitonDuration: '0ms'
+    }
+    middleRStyle.value = {
+      transform: `translate3D(${offsetWidth}px,0,0)`,
+      transitonDuration: '0ms'
+    }
   }
-  function onMiddleTouchEnd() { }
+  function onMiddleTouchEnd() {
+    let offsetWidth
+    let opacity
+    if (currentShow.value === 'cd') {
+      currentView = 'cd'
+      offsetWidth = 0
+      opacity = 1
+    } else {
+      offsetWidth = -window.innerWidth
+      opacity = 0
+    }
+    const duration = 300
+    // edit style
+    middleLStyle.value = {
+      opacity,
+      transitonDuration: `${duration}ms`
+    }
+    middleRStyle.value = {
+      transform: `translate3D(${offsetWidth}px,0,0)`,
+      transitonDuration: `${duration}ms`
+    }
+  }
   return {
+    currentShow,
+    middleLStyle,
+    middleRStyle,
     onMiddleTouchStart,
     onMiddleTouchMove,
     onMiddleTouchEnd
