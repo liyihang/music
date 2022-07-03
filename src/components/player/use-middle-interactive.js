c9e2e136a7bc58686dfed66873ded10769a7fc50
lidoudoou <lidoudoou@gmail.com>
2022-07-03 23:59:44 +0800

    change mode

--- src/components/player/use-middle-interactive.js
+++ src/components/player/use-middle-interactive.js
@@ -3,7 +3,26 @@ export default function useMiddleInterActive() {
   const currentShow = ref('cd')
   const middleLStyle = ref(null)
   const middleRStyle = ref(null)
-  function onMiddleTouchStart() { }
-  function onMiddleTouchMove() { }
+  const touch = {}
+  let currentView = 'cd'
+  function onMiddleTouchStart(e) {
+    touch.startX = e.touches[0].pageX
+  }
+  function onMiddleTouchMove(e) {
+    const deltaX = e.touches[0].pageX - touch.startX
+    const left = currentView === 'cd' ? 0 : -window.innerWidth
+    const offsetWidth = Math.min(0, Math.max(-window.innerWidth, left + deltaX))
+    touch.percent = Math.abs(offsetWidth / window.innerWidth)
+    if (currentView == 'cd') {
+      if (touch.percent > 0.2) {
+        touc
+      }
+    }
+  }
   function onMiddleTouchEnd() { }
+  return {
+    onMiddleTouchStart,
+    onMiddleTouchMove,
+    onMiddleTouchEnd
+  }
 }
